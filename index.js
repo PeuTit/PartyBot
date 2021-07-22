e08d9c037ab58df01c270006b3284278f3ffc9eb
Titouan <22703571+LilTits@users.noreply.github.com>
2021-07-22 15:03:45 +0200

    Add enteraction to the bot

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
-// ... client setup (keep reading)
-client.login(process.env.TOKEN);
 
+const { prefix }= require('./config.json')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -10,4 +9,55 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login('your-token-goes-here');
+client.on('message', message => {
+  if (!message.content.startsWith(`${prefix}`) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (command === 'tki?') {
+    message.channel.send('toi t ki?')
+  } else if (command === 'tcon?') {
+    message.channel.send("Moi non mais Sacha oui. C'est pas la moitie d'un con je peux te le dire")
+  } else if (command === 'pyramide') {
+    message.channel.send("Let's go")
+    message.channel.send('?')
+    message.channel.send('??')
+    message.channel.send('???')
+    message.channel.send('??')
+    message.channel.send('?')
+    message.channel.send("J'ai la flemme de faire plus ok!")
+  } else if (command === 'server') {
+    message.channel.send(`${message.guild.name} Member: ${message.guild.memberCount}`)
+  } else if (command === 'args-info') {
+    if (!args.length) {
+      return message.channel.send(`No arguments ${message.author.username}`)
+    } else if (args[0] == 'foo') {
+      message.channel.send('bar')
+    }
+    message.channel.send(`Command: ${command} Arguments: ${args}`)
+  } else if (command === 'kick') {
+    const taggedUser = message.mentions.users.first()
+
+    if (!taggedUser) {
+      return message.reply('You must tag an user in order to kick them')
+    }
+
+    message.channel.send(`You want to kick this son of a dog? ${taggedUser.username}`)
+  } else if (command === 'avatar') {
+    const taggedUser = message.mentions.users.first()
+
+    if (!taggedUser) {
+      return message.reply('You must tag an user in order to kick them')
+    }
+
+    message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'gif', dynamic: true})}>`)
+
+  }
+
+  // if (message.author.id === '279999507327090689') {
+  //   message.channel.send(`Salut ${message.author.username}`)
+  // }
+})
+
+client.login(process.env.TOKEN);
