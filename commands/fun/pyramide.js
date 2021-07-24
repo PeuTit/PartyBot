7c8389b143865ddcae47ed918125494ef45df9ea
Titouan <22703571+LilTits@users.noreply.github.com>
2021-07-24 21:08:07 +0200

    Update the pyramide to use args provided by user

--- commands/fun/pyramide.js
+++ commands/fun/pyramide.js
@@ -1,13 +1,25 @@
 module.exports = {
-	name: 'pyramide',
-	description: 'Create a pyramide',
-	execute(message) {
-    message.channel.send("Let's go")
-    message.channel.send('?')
-    message.channel.send('??')
-    message.channel.send('???')
-    message.channel.send('??')
-    message.channel.send('?')
-    message.channel.send("J'ai la flemme de faire plus ok!")
-	},
+  name: "pyramide",
+  description: "Create a pyramide",
+  usage: "command <user> <size>",
+  execute(message, args) {
+    const data = [];
+
+    if (!args.length) {
+      return message.reply("You need to tag an user!");
+    }
+
+    const pyramideSize = args[1] || 5;
+
+    for (let index = 1; index < pyramideSize; index++) {
+      data.push(args[0].repeat(index));
+    }
+
+    const reverseData = [...data];
+    data.push(args[0].repeat(args[1]));
+    reverseData.reverse();
+    const finalData = [...data, ...reverseData];
+
+    return message.channel.send(finalData, { split: true });
+  },
 };
