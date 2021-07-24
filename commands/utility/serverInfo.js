e9af205c2f23bf336bfcf60110c08858c4790690
Titouan <22703571+LilTits@users.noreply.github.com>
2021-07-24 10:57:47 +0200

    Add alias to other command

--- commands/utility/serverInfo.js
+++ commands/utility/serverInfo.js
@@ -1,7 +1,11 @@
 module.exports = {
-	name: 'server-info',
-	description: 'Get some info about the server!',
-	execute(message) {
-    message.channel.send(`${message.guild.name} Member: ${message.guild.memberCount}`)
-	},
+  name: "server-info",
+  aliases: ["info"],
+  guildOnly: true,
+  description: "Get some info about the server!",
+  execute(message) {
+    message.channel.send(
+      `${message.guild.name} Member: ${message.guild.memberCount}`
+    );
+  },
 };
