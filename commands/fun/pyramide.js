a113b78685bdf2297a87c98831e36b1985a173de
Titouan <22703571+LilTits@users.noreply.github.com>
2021-07-25 12:26:24 +0200

    Minor updates to the pyramide command

--- commands/fun/pyramide.js
+++ commands/fun/pyramide.js
@@ -2,21 +2,23 @@ module.exports = {
   name: "pyramide",
   description: "Create a pyramide",
   usage: "command <user> <size>",
+  args: true,
   execute(message, args) {
     const data = [];
+    const defaultSize = 5;
 
     if (!args.length) {
       return message.reply("You need to tag an user!");
     }
 
-    const pyramideSize = args[1] || 5;
+    const pyramideSize = args[1] || defaultSize;
 
     for (let index = 1; index < pyramideSize; index++) {
       data.push(args[0].repeat(index));
     }
 
     const reverseData = [...data];
-    data.push(args[0].repeat(args[1]));
+    data.push(args[0].repeat(args[1] || defaultSize));
     reverseData.reverse();
     const finalData = [...data, ...reverseData];
 
