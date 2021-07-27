c272281d3bd7b936aba77e6ae9e8eeda7310f584
Titouan <22703571+LilTits@users.noreply.github.com>
2021-07-27 14:08:24 +0200

    refactor index.js

--- index.js
+++ index.js
@@ -7,26 +7,31 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const eventPath = "./events";
+const commandPath = "./commands";
+const fileType = ".js";
+
 const eventFiles = fs
-  .readdirSync("./events")
-  .filter((file) => file.endsWith(".js"));
+  .readdirSync(eventPath)
+  .filter((file) => file.endsWith(fileType));
 
 for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
+  const event = require(`${eventPath}/${file}`);
   if (event.once) {
     client.once(event.name, (...args) => event.execute(...args, client));
   } else {
     client.on(event.name, (...args) => event.execute(...args, client));
   }
 }
-const commandFolders = fs.readdirSync("./commands");
+
+const commandFolders = fs.readdirSync(commandPath);
 
 for (const folder of commandFolders) {
   const commandFiles = fs
-    .readdirSync(`./commands/${folder}`)
-    .filter((file) => file.endsWith(".js"));
+    .readdirSync(`${commandPath}/${folder}`)
+    .filter((file) => file.endsWith(fileType));
   for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
+    const command = require(`${commandPath}/${folder}/${file}`);
     client.commands.set(command.name, command);
   }
 }
